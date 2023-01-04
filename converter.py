c11fc868da89ab433ec43ae9f576e0cf2f4bde39
Trenord <luca.ilari@gmail.com>
2023-01-04 22:03:21 +0100

    test
    just a test

--- converter.py
+++ converter.py
@@ -4,7 +4,7 @@ import json
 import time
 
 # Download the HTML source code for the page
-r = requests.get("https://en.wikipedia.org/wiki/List_of_IBA_official_cocktails")
+r = requests.get("https://iba-world.com/category/iba-cocktails/")
 
 # Parse the HTML with BeautifulSoup
 soup = BeautifulSoup(r.text, "html.parser")
@@ -13,7 +13,7 @@ soup = BeautifulSoup(r.text, "html.parser")
 #content = soup.find(id="main")
 
 # Find all of the cocktail cards
-cocktail_cards = soup.find_all("dt")
+cocktail_cards = soup.find_all("article")
 
 # Create an empty list to store the cocktail dictionaries
 cocktails = []
@@ -22,57 +22,42 @@ cocktails = []
 
 for card in cocktail_cards:
     ingredients = []
-    metods = []
 
-    name = card.text.strip()
+    CocktailLink=card.find("h3").find('a')
+
+    name=CocktailLink.text.strip()
     print(name)
-    if name != "Spicy fifty":
-        CocktailLink=card.find("a")
-        Cr = requests.get("https://en.wikipedia.org" + CocktailLink.attrs["href"])
-        CocktailSoup = BeautifulSoup(Cr.text, "html.parser")
-        #lista ingredienti non formattata
-        Prep_Ingr = CocktailSoup.table.find_all("tr")
 
-        #Filter only ingredients
-        
-        for ingredienti in Prep_Ingr:
-            if "ingredients" in ingredienti.text:
-                for singolo in ingredienti.find_all("li"):
-                    #print(singolo)
-                    if singolo.find("\u00a0") != -1:
-                        singolo = singolo.text.replace("\u00a0"," ")
-                        print(singolo)
-                    elif singolo.find("\u00e8"):
-                        singolo = singolo.text.replace("\u00e8","è")
-                        print(singolo)
-                    else:
-                        singolo = singolo.text
-                    ingredients.append(singolo)
-
-        #Filter preparation
-        for preparation in Prep_Ingr:
-            if "Preparation" in preparation.text:
-                metods.append(preparation.text)
-
-        cocktail = {
-            "name": name,
-            "ingredients": ingredients,
-            "metods": metods
-        }
-        
-
+    Cr = requests.get(CocktailLink.attrs["href"])
+    CocktailSoup = BeautifulSoup(Cr.text, "html.parser")
+    #lista ingredienti non formattata
+    Prep_Ingr = CocktailSoup.table.find_all("h3")
     
-    if name == "Spicy fifty":
-        cocktail = {
-            "name": "Spicy fifty",
-            "ingredients": "",
-            "metods": "metods"
-        }
+    #Filter only ingredients
     
+    for ingredienti in Prep_Ingr:
+        if "ingredients" in ingredienti.text:
+            for singolo in ingredienti.find_all("li"):
+                singolo = singolo.text
+                ingredients.append(singolo)
+
+    #Filter preparation
+    for preparation in Prep_Ingr:
+        if "Preparation" in preparation.text:
+            metods = preparation.text.replace("Preparation","")
+
+    cocktail = {
+        "name": name,
+        "ingredients": ingredients,
+        "metods": metods,
+        "note": note,
+        "garnish": garnish
+    }
     cocktails.append(cocktail)
-    time.sleep(0.5)
+    break
+    time.sleep(10.0)
     
-print(len(cocktails))
+#print(len(cocktails))
 
 # Convert the list of cocktails to a JSON string
 json_string = json.dumps(cocktails)
